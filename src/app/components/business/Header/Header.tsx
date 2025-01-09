83a636a29c9383080dff13962cda86c1d4a124f1
ChmyrRoma <chmyrroma.2002@gmail.com>
2025-01-09 12:11:30 +0200

    fix styles, add active router

--- src/app/components/business/Header/Header.tsx
+++ src/app/components/business/Header/Header.tsx
@@ -1,17 +1,30 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Stack } from "@mui/material";
 
-
-import styles from './header.module.scss';
+import styles from "./header.module.scss";
 
 const Header = () => {
+    const pathname = usePathname();
 
     return (
         <Stack direction="row" justifyContent="center" spacing={2} className={styles.header}>
-            <Link href="/edit" className={styles.header__button}>Edit Users</Link>
-            <Link href="/" className={styles.header__button}>Users</Link>
+            <Link
+                href="/edit"
+                className={`${styles.header__button} ${pathname === "/edit" ? styles.header__active : ""}`}
+            >
+                Edit Users
+            </Link>
+            <Link
+                href="/"
+                className={`${styles.header__button} ${pathname === "/" ? styles.header__active : ""}`}
+            >
+                Users
+            </Link>
         </Stack>
-    )
-}
+    );
+};
 
 export default Header;
